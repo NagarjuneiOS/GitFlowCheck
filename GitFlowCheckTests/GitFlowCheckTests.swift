aca21cbd2f8977cb129d4b5542ca2226646e0651
Nagarjune <nagarjuneofficial@gmail.com>
2025-08-09 09:48:50 +0530

    source initial commit - Nagarjune

---
+++ GitFlowCheckTests/GitFlowCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowCheckTests.swift
+//  GitFlowCheckTests
+//
+//  Created by Nagarjune M on 09/08/25.
+//
+
+import Testing
+@testable import GitFlowCheck
+
+struct GitFlowCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ GitFlowCheckUITests/GitFlowCheckUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitFlowCheckUITestsLaunchTests.swift
+//  GitFlowCheckUITests
+//
+//  Created by Nagarjune M on 09/08/25.
+//
+
+import XCTest
+
+final class GitFlowCheckUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
